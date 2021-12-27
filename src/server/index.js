6a57c61bff94e36e0c35b50be289a2530a9d4a72
taiyo <nishimototai@gmail.com>
2021-12-27 14:07:52 +0900

    setup the env for api key

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8081;
+const dotenv = require('dotenv');
+dotenv.config();
+
+var textapi = new api({
+    api_key: process.env.API_KEY
+})
 
 app.listen(port, () => {
     console.log(`You are on the port : ${port}`);
